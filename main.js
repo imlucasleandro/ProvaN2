28396ad5482c17d3d0f42d2b403c7b6021f8cf51
Lucas Leandro <110395319+imlucasleandro@users.noreply.github.com>
2023-06-17 09:26:01 -0400

    main1.js

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const modulo1 = require('./moduloexercicio1')
 const modulo2 = require('./moduloexercicio2')
+const modulo3 = require('./moduloexercicio3')
+const modulo4 = require('./moduloexercicio4')
 
 const matriz = [
  [1,2,3,4,5],
@@ -11,3 +13,7 @@ const soma = modulo1.adicao(matriz)
 console.log(soma)
 const somaexp  = modulo2.adicaoexp(matriz)
 console.log(somaexp)
+const medialc = modulo3.media(matriz)
+console.log(medialc)
+const multipli = modulo4.multipicar(matriz)
+console.log(multipli)
